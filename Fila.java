839f50a26f2fc7293ad278fb9773f13ca6cd33b7
Gustavo Galli <gustavo.galli@elo7.com>
2021-06-16 13:04:06 -0300

    Adicionando o primeiro commit

---
+++ Fila.java
@@ -0,0 +1,71 @@
+package filas;
+
+public class Fila {
+	int head;
+	int tail;
+	int tamanho;
+	int usada;
+	int [] arrayFila;
+	// int [] fila = new int [tamanho da fila];
+	// fila[2] = 10.0;
+	
+	// construtor
+	public Fila(int tamanho) {
+		this.tamanho = tamanho;
+		this.head = 0;
+		this.tail = -1;
+		this.usada = 0;
+		arrayFila = new int[tamanho];
+	}
+
+	public void adicionar(int item) {
+		this.tail = (this.tail + 1) % this.tamanho; // aumenta 1 na posição TAIL
+		System.out.println("Adicionando o item " + item + " na posição " + this.tail); // imprime
+		this.arrayFila[this.tail] = item; // adiciona ITEM na ARRAYFILA no indice TAIL
+		this.usada = this.usada + 1; // aumenta 1 na quantidade usada
+//		if () {} else {}
+		
+	}
+	
+	public int pegar() {
+		return this.arrayFila[this.head]; // pega o item na posição HEAD da ARRAYFILA
+	}
+	
+	public void deletar() {}
+	
+	public void estaVazia() {
+		if (this.usada == 0) {
+			System.out.println("A fila está vazia.");
+		} else {
+			System.out.println("A fila não está vazia.");
+		}
+	}
+	
+	public void estaCheia() {
+		if (this.usada == this.tamanho) {
+			System.out.println("A fila está cheia.");
+		} else {
+			System.out.println("A fila não está cheia.");
+		}
+	}
+	
+	public void quantosNaFila() {
+		if (this.usada == 1) {
+			System.out.println("Temos 1 item na fila. Capacidade: " + this.tamanho);
+		} else {
+		System.out.println("Temos " + this.usada + " itens na fila. Capacidade: " + this.tamanho);
+		}
+	}
+	
+	public void status() {
+		System.out.print("LISTA: ");
+		for(int item: arrayFila) {
+		System.out.print(item + " ");
+		}
+		System.out.println();
+		}
+
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package filas;
+public class Main {
+	public static void main(String[] args) {
+		Fila f = new Fila(5);
+		f.adicionar(99);
+		f.adicionar(12);
+		f.adicionar(45);
+		f.adicionar(32);
+		f.adicionar(81);	
+//		f.adicionar(13);
+//		f.quantosNaFila();
+//		f.status();
+//		f.quantosNaFila();
+		f.estaCheia();
+//		f.estaVazia();
+		
+		
+	}
+}
